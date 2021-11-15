0cc350cbdabda29bb26ddd14bcbdb02d1ffcfdd9
BernardoMar <bernardomartin92@outlook.com>
2021-11-15 11:00:00 +1100

    Monday Morning

--- apiKeySinatra.rb
+++ apiKeySinatra.rb
@@ -4,6 +4,12 @@ require "sinatra/cross_origin"
 require "json"
 require "httparty"
 require 'rest-client'
+require 'protobuf'
+require 'google/transit/gtfs-realtime.pb'
+require 'uri'
+require 'net/http'
+# require 'google-protobuf'
+
 
 set :bind, '0.0.0.0'
 configure do
@@ -20,12 +26,23 @@ options "*" do
   200
 end
 
-get "/" do
-
-  results = HTTParty.get("https://api.transport.nsw.gov.au/v1/gtfs/vehiclepos/sydneytrains?debug=true", :headers => {
+def get_results
+  results = Net::HTTP.get(URI.parse("https://api.transport.nsw.gov.au/v1/gtfs/vehiclepos/nswtrains"), :headers => {
   "Authorization" => "apikey 2rZpu5FuWGpahN4FBDm5rz7CFBIddMjeYKwf"
 })
+  feed = Transit_realtime::FeedMessage.decode(results)
+  trip_data = []
+  for entity in feed.entity do
+    if entity.field?(:trip_update)
+      trip_data << entity.trip_update
+    end
+  end
+  trip_data.to_json
 
-results.parsed_response;
+end
 
+get "/" do
+
+  get_results
+  # results.parsed_response;
 end
