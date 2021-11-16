ce25a136d1680f1598f83449f04fa1c6bead2342
Derek Wei <derek.wei94@gmail.com>
2021-11-16 13:11:26 +1100

    added route_id, and time, renamed trip_id to id

--- apiKeySinatra.rb
+++ apiKeySinatra.rb
@@ -12,6 +12,7 @@ set :bind, '0.0.0.0'
 configure do
   enable :cross_origin
 end
+
 before do
   response.headers['Access-Control-Allow-Origin'] = '*'
 end
@@ -25,54 +26,64 @@ end
 
 get "/" do
   results = HTTParty.get("https://api.transport.nsw.gov.au/v1/gtfs/vehiclepos/sydneytrains?debug=true", :headers => {
-  "Authorization" => "apikey 2rZpu5FuWGpahN4FBDm5rz7CFBIddMjeYKwf"
-})
-  labels = [];
-  longituds = [];
-  latituds = [];
-  dataInJson = results.parsed_response;
-  dataInJson.each_line do |line|
-    longituds << line if line.include?("longitud")
-    latituds << line if line.include?("latitud")
+    "Authorization" => "apikey 2rZpu5FuWGpahN4FBDm5rz7CFBIddMjeYKwf"
+  })
+
+  data_in_JSON = results.parsed_response;
+
+  labels = []
+  longitudes = []
+  latitudes = []
+  route_ids = []
+
+  data_in_JSON.each_line do |line|
+    longitudes << line if line.include?("longitude")
+    latitudes << line if line.include?("latitude")
     labels << line if line.include?("label")
+    route_ids << line if line.include?("route_id")
   end
 
-    #Need to clean up the data in each array.
-   cleanLongituds = longituds.map { |coordinate| coordinate[17..23].to_f }
-   cleanLatituds = latituds.map { |coordinate| coordinate[16..23].to_f }
-   cleanLabelsOrigin = labels.map { |stations| stations[20...-3].split.first }
-   cleanLabelsDestination = labels.map { |stations| stations[20...-3].chomp("Station").split.last }
-   #loop through each and create an array of hashes.
+  # Need to clean up the data in each array.
+  clean_longitudes = longitudes.map { |coordinate| coordinate[17..23].to_f }
+  clean_latitudes = latitudes.map { |coordinate| coordinate[16..23].to_f }
+  clean_labels_origin = labels.map { |label| label[20...-3].partition(" to ")[0] }
+  clean_labels_destination = labels.map { |label| label[20...-3].partition(" to ")[2] }
+  clean_labels_time = labels.map { |label| label[14...19] }
+  clean_route_id = route_ids.map { |route_id| route_id.scan(/"([^"]*)"/)[0][0] }
 
-   # Empty object
+  # loop through each and create an array of hashes.
+  result_array = []
 
-   # trainTrips = {}
-   resultArray = [];
   i = 0
   loop do
-     resultArray[i]= {
-       "origin" => cleanLabelsOrigin[i],
-       "destination" => cleanLabelsDestination[i],
-       "lat" => cleanLatituds[i],
-       "lng" => cleanLongituds[i],
-       "trip_id" => i+1,
-     }
-     # trainTrips[:origin] =  cleanLabelsOrigin[i]
-     # trainTrips[:destination] = cleanLabelsDestination[i]
-     # trainTrips[:latitud] = cleanLatituds[i]
-     # trainTrips[:longitud] = cleanLongituds[i]
-     # trainTrips[:trip_id] =  i+1
-     # resultArray.push(trainTrips)
+    result_array[i] = {
+      "id" => i + 1,
+      "time" => clean_labels_time[i],
+      "origin" => clean_labels_origin[i],
+      "destination" => clean_labels_destination[i],
+      "lat" => clean_latitudes[i],
+      "lng" => clean_longitudes[i],
+      "route_id" => clean_route_id[i],
+    }
+
     i = i + 1
     if i == labels.length();
-     break       # this will cause execution to exit the loop
-   end
+      break       # this will cause execution to exit the loop
+    end
   end
 
-finalArray = resultArray.to_json
+  final_array = result_array.to_json
 
-finalArray
+  final_array
+end
+
+get "/updates" do
+  results = HTTParty.get("https://api.transport.nsw.gov.au/v1/gtfs/realtime/sydneytrains?debug=true", :headers => {
+    "Authorization" => "apikey r8aueiiLOTKZSGo91lOOiktLtcySJeXaZyM5"
+  })
+  results.parsed_response
+
+  data_in_JSON = results.parsed_response;
 
 
- # binding.pry
- end
+end
